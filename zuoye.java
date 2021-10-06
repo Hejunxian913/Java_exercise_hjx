dbb4e168b8342c5ca90c992fa605cf506423d0be
hejunxian <hejunxian912@gmail.com>
2021-10-06 19:22:44 +0900

    first commit zuoye

---
+++ zuoye.java
@@ -0,0 +1,24 @@
+package basic;
+
+public class zuoye {
+
+  public static void main(String[] args) {
+
+
+    byte b;
+    short s;
+    int i;
+    long l;
+    float f;
+    double d;//様々な変数を宣言し初期値を与えない。
+
+
+    /*System.out.println(b);
+    System.out.println(s);
+    System.out.println(i);
+    System.out.println(l);
+    System.out.println(f);
+    System.out.println(d); //変数の値を出力する。*/
+  }
+}
+
